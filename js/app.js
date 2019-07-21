a588096f9f241eecb76d0b074611118d5e4b3eac
Tobiasz Nartowski <tobiahao@gmail.com>
2019-07-21 12:21:02 +0200

    Added form validation

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Forms validation
+window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('form-validated');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            for (let item of form.children) {
+                if (!item.classList.contains('disable-validation')) {
+                    item.classList.add('was-validated');
+                }
+            }
+        }, false);
+    });
+}, false);
