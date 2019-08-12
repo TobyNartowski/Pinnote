b444e3bd473ee0d90161d93b83fe618fbfb7af3f
TobyNartowski <tobiasznartowski@gmail.com>
2019-08-12 19:09:55 +0200

    Moving static files to java project

---
+++ src/main/java/pl/tobynartowski/pinnote/PinnoteApplication.java
@@ -0,0 +1,13 @@
+package pl.tobynartowski.pinnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinnoteApplication.class, args);
+    }
+
+}
